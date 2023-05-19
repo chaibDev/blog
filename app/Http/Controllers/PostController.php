daaed6fe5ef6bbdc222530c11564421ac32d7233
chaibDev <cnechaib@hotmail.fr>
2023-05-19 21:16:07 +0200

    debut

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use illuminate\View\View;
+
+class PostController extends Controller
+{
+    public function index(): View
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate(10),
+        ]);
+       
+    }
+
+    public function show(Post $post): View
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+}
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog Laravel</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css' />
+  <link rel='stylesheet'
+    href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css' />
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.css" />
+
+</head>
+
+
+<p style="margin-lef:80px;">{{$post->id}}</p>
+<h2><p style="margin-lef:80px;margin-right:30%;">{{$post->title}}</p></h2>
+<br>
+<p style="margin-lef:80px;"><img src="{{$post->thumbnail}}"></a></p>
+<br>
+<p style="margin-lef:80px;margin-right:30%;">{{$post->content}}</p>
+    
+<a href="/posts" class="btn btn-primary">Liste des articles</a>
